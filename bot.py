886fd23e47395fb45f6830074925ea85add1f05e
astvoff <astv.off@gmail.com>
2025-07-07 15:03:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,15 +10,14 @@ TOKEN = os.environ.get('TOKEN')
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Бот працює!")
 
-async def main():
+def main():
     app = ApplicationBuilder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
 
     logging.info("Бот запущено.")
 
-    await app.run_polling()
+    app.run_polling()
 
 if __name__ == '__main__':
-    import asyncio
-    asyncio.run(main())
+    main()
